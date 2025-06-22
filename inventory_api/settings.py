97e314837998f1bf29134578ada25b5e7fbafaaf
giteshwari16 <gitp1609@gmail.com>
2025-06-22 14:06:17 +0530

    Fix: Add recommended CORS settings for production

--- inventory_api/settings.py
+++ inventory_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,6 +60,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://inventory-frontend-1lo2rztri-giteshwaris-projects.vercel.app',
 ]
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'access-control-allow-origin',
+]
+CORS_ALLOW_ALL_METHODS = True
 
 
 ROOT_URLCONF = 'inventory_api.urls'
